bb221755e0dba43f8bb6d1a3c98cdbb55b56a16c
Inprodi Web Studio <proyectos@inprodi.mx>
2025-07-01 12:50:27 -0600

    find favorites route

--- src/api/favorite/controllers/favorite.js
+++ src/api/favorite/controllers/favorite.js
@@ -1,6 +1,15 @@
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::favorite.favorite', ({ strapi }) => ({
+    async find(ctx) {
+        const { user } = ctx.state || {};
+
+        return await strapi.db.query('api::favorite.favorite').findMany({
+            where: {
+                user: user?.id,
+            },
+        });
+    },
     async create(ctx) {
         const { user } = ctx.state || {};
         const { productId } = ctx.request.body || {};
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -427,10 +427,7 @@ export interface ApiFavoriteFavorite extends Struct.CollectionTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    user: Schema.Attribute.Relation<
-      'oneToOne',
-      'plugin::users-permissions.user'
-    >;
+    user: Schema.Attribute.Relation<'oneToOne', 'admin::user'>;
   };
 }
 
