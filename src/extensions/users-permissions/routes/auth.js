aa88e8e749939b0c336345d35b7fc2771eb384f7
Inprodi Web Studio <proyectos@inprodi.mx>
2025-07-03 14:18:55 -0600

    fix auth routes

--- src/extensions/users-permissions/routes/auth.js
+++ src/extensions/users-permissions/routes/auth.js
@@ -1,51 +1,51 @@
-module.exports = ( plugin ) => {
-    plugin.routes["content-api"].routes.push({
-        method  : "POST",
-        path    : "/auth/login",
-        handler : "user.login",
-        config  : {
-            auth : false,
-            prefix : "",
-        },
-    });
+module.exports = (plugin) => {
+  plugin.routes["content-api"].routes.push({
+    method: "POST",
+    path: "/auth/login",
+    handler: "user.login",
+    config: {
+      auth: false,
+      prefix: "",
+    },
+  });
 
-    plugin.routes["content-api"].routes.push({
-        method  : "POST",
-        path    : "/auth/register",
-        handler : "user.register",
-        config  : {
-            auth : false,
-            prefix : "",
-        },
-    });
+  plugin.routes["content-api"].routes.push({
+    method: "POST",
+    path: "/auth/register",
+    handler: "user.register",
+    config: {
+      auth: false,
+      prefix: "",
+    },
+  });
 
-    plugin.routes["content-api"].routes.push({
-        method  : "POST",
-        path    : "/auth/forgot-password",
-        handler : "user.forgotPassword",
-        config  : {
-            auth : false,
-            prefix : "",
-        },
-    });
+  plugin.routes["content-api"].routes.push({
+    method: "POST",
+    path: "/auth/forgot",
+    handler: "user.forgotPassword",
+    config: {
+      auth: false,
+      prefix: "",
+    },
+  });
 
-    plugin.routes["content-api"].routes.push({
-        method  : "POST",
-        path    : "/auth/validate-code",
-        handler : "user.validateCode",
-        config  : {
-            auth : false,
-            prefix : "",
-        },
-    });
+  plugin.routes["content-api"].routes.push({
+    method: "POST",
+    path: "/auth/validate",
+    handler: "user.validateCode",
+    config: {
+      auth: false,
+      prefix: "",
+    },
+  });
 
-    plugin.routes["content-api"].routes.push({
-        method  : "POST",
-        path    : "/auth/reset-password",
-        handler : "user.resetPassword",
-        config  : {
-            auth : false,
-            prefix : "",
-        },
-    });
-}
+  plugin.routes["content-api"].routes.push({
+    method: "POST",
+    path: "/auth/reset",
+    handler: "user.resetPassword",
+    config: {
+      auth: false,
+      prefix: "",
+    },
+  });
+};
