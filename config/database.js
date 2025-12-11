77f4e41d1041017adf12fef5758299f6e85a1996
Inprodi Web Studio <proyectos@inprodi.mx>
2025-12-11 12:22:20 -0600

    pool fix

--- config/database.js
+++ config/database.js
@@ -20,7 +20,16 @@ module.exports = ({ env }) => {
           rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
         },
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      pool: {
+        min: env.int('DATABASE_POOL_MIN', 0),
+        max: env.int('DATABASE_POOL_MAX', 30),
+        acquireConnectionTimeout: env.int('DATABASE_ACQUIRE_TIMEOUT', 20000),
+        createTimeoutMillis: env.int('DATABASE_CREATE_TIMEOUT', 30000),
+        destroyTimeoutMillis: env.int('DATABASE_DESTROY_TIMEOUT', 5000),
+        idleTimeoutMillis: env.int('DATABASE_IDLE_TIMEOUT', 30000),
+        reapIntervalMillis: env.int('DATABASE_REAP_INTERVAL', 1000),
+        createRetryIntervalMillis: env.int('DATABASE_RETRY_INTERVAL', 200)
+      },
     },
     postgres: {
       connection: {
